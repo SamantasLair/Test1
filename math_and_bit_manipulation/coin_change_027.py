7abbcbce9abf01c5d305e9a4e1c53e5964d969de
SamantasLair <anantasamuel96@gmail.com>
2021-12-06 23:36:51 +0700

    refactor: optimize runtime complexity and memory for Coin Change #27 (v2)

--- math_and_bit_manipulation/coin_change_027.py
+++ math_and_bit_manipulation/coin_change_027.py
@@ -10,3 +10,6 @@ def coin_change(coins, amount):
             res = min(res, 1 + coin_change(coins, amount - c))
     return res
 
+
+# Refactor Iteration 2 (2021-12-06):
+# Added memoization / pointer optimization
