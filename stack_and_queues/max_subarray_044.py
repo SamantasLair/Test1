5de131811623ba87376881e8914f54f06ce7da92
SamantasLair <anantasamuel96@gmail.com>
2021-12-07 01:47:10 +0700

    feat: implement initial naive solution for Maximum Subarray #44

---
+++ stack_and_queues/max_subarray_044.py
@@ -0,0 +1,14 @@
+# Problem: Maximum Subarray #44
+# Description: Find contiguous subarray with largest sum
+# Iteration: 1
+
+def max_sub(nums):
+    max_s = nums[0]
+    for i in range(len(nums)):
+        cur = 0
+        for j in range(i, len(nums)):
+            cur += nums[j]
+            if cur > max_s:
+                max_s = cur
+    return max_s
+
