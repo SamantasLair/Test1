96946954ad78481a6b66de13e9750dd1d0de88ad
SamantasLair <anantasamuel96@gmail.com>
2022-01-09 20:36:17 +0700

    feat: implement initial naive solution for Reverse Linked List #67

---
+++ trees_and_graphs/reverse_linked_list_067.py
@@ -0,0 +1,21 @@
+# Problem: Reverse Linked List #67
+# Description: In-place iterative reversal of singly linked list
+# Iteration: 1
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverse_list(head):
+    vals = []
+    cur = head
+    while cur:
+        vals.append(cur.val)
+        cur = cur.next
+    cur = head
+    for v in reversed(vals):
+        cur.val = v
+        cur = cur.next
+    return head
+
