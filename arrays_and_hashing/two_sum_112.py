22fb11cf27dae89982de1409d00bc57b100396e4
SamantasLair <anantasamuel96@gmail.com>
2022-03-03 20:54:03 +0700

    feat: implement initial naive solution for Two Sum #112

---
+++ arrays_and_hashing/two_sum_112.py
@@ -0,0 +1,11 @@
+# Problem: Two Sum #112
+# Description: Find indices of two numbers that add up to target
+# Iteration: 1
+
+def solve(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
+
