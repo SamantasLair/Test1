e6e5668fb4d95bb5d7572cabc4857467f666e5f7
SamantasLair <anantasamuel96@gmail.com>
2022-05-27 19:29:17 +0700

    feat: implement initial naive solution for Climbing Stairs #173

---
+++ dynamic_programming/climbing_stairs_173.py
@@ -0,0 +1,8 @@
+# Problem: Climbing Stairs #173
+# Description: Calculate distinct ways to climb n steps
+# Iteration: 1
+
+def climb(n):
+    if n <= 2: return n
+    return climb(n - 1) + climb(n - 2)
+
