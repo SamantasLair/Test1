a5a3ca18add1c800ccd5e499df91ecadf214ec71
SamantasLair <anantasamuel96@gmail.com>
2022-06-10 20:09:48 +0700

    refactor: optimize runtime complexity and memory for Binary Search #136 (v3)

--- binary_search/binary_search_136.py
+++ binary_search/binary_search_136.py
@@ -11,3 +11,6 @@ def search(nums, target):
 
 # Refactor Iteration 2 (2022-04-12):
 # Added memoization / pointer optimization
+
+# Refactor Iteration 3 (2022-06-10):
+# Added memoization / pointer optimization
