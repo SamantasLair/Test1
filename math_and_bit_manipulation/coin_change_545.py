61fa86bc180fbf3a3c3f3cb08b70e4ebe5edfb57
SamantasLair <anantasamuel96@gmail.com>
2023-07-10 02:51:59 +0700

    feat: implement initial naive solution for Coin Change #545

---
+++ math_and_bit_manipulation/coin_change_545.py
@@ -0,0 +1,12 @@
+# Problem: Coin Change #545
+# Description: Fewest number of coins needed to make up amount
+# Iteration: 1
+
+def coin_change(coins, amount):
+    if amount == 0: return 0
+    res = float('inf')
+    for c in coins:
+        if amount - c >= 0:
+            res = min(res, 1 + coin_change(coins, amount - c))
+    return res
+
