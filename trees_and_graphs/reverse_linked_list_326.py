3704127f03123aef147a8429761bc8cb82624a0b
SamantasLair <anantasamuel96@gmail.com>
2024-03-08 23:56:27 +0700

    refactor: optimize runtime complexity and memory for Reverse Linked List #326 (v3)

--- trees_and_graphs/reverse_linked_list_326.py
+++ trees_and_graphs/reverse_linked_list_326.py
@@ -22,3 +22,6 @@ def reverse_list(head):
 
 # Refactor Iteration 2 (2023-03-24):
 # Added memoization / pointer optimization
+
+# Refactor Iteration 3 (2024-03-08):
+# Added memoization / pointer optimization
