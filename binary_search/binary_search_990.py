dee6066dd12ec41be1e3eb33af0942bce998429b
SamantasLair <anantasamuel96@gmail.com>
2024-12-30 20:46:20 +0700

    feat: implement initial naive solution for Binary Search #990

---
+++ binary_search/binary_search_990.py
@@ -0,0 +1,10 @@
+# Problem: Binary Search #990
+# Description: Search target in sorted array in O(log n)
+# Iteration: 1
+
+def search(nums, target):
+    for idx, val in enumerate(nums):
+        if val == target:
+            return idx
+    return -1
+
